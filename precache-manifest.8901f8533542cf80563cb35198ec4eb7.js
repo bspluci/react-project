16ccfb2dc8bcb412e92e0a94837cb9d71cb2addd
bspluci <47733415+bspluci@users.noreply.github.com>
2020-03-22 00:53:35 +0900

    Updates

---
+++ precache-manifest.8901f8533542cf80563cb35198ec4eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3bb235ceed43cb7efb8e73e85eebed",
+    "url": "/bspluci/my-homework.git/index.html"
+  },
+  {
+    "revision": "04dd8e2ac5243f2c8843",
+    "url": "/bspluci/my-homework.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99715d6afa84c973ce84",
+    "url": "/bspluci/my-homework.git/static/js/2.4ed9c875.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bspluci/my-homework.git/static/js/2.4ed9c875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dd8e2ac5243f2c8843",
+    "url": "/bspluci/my-homework.git/static/js/main.4043ae6a.chunk.js"
+  },
+  {
+    "revision": "fef42997e8791a82203d",
+    "url": "/bspluci/my-homework.git/static/js/runtime-main.91b94c48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bspluci/my-homework.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
