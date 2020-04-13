aad894f350a9efa7259941e416a57518b3078cbc
pjh <pjh@gmail.com>
2020-04-13 19:10:43 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
 import React, { Component } from "react";
 
 class App extends Component {
-   state = {
-      userid: "",
-      password: "",
-      email: "",
-      disabled: true,
-   };
-
-   handleChange = e => {
-      const { value, name } = e.target;
-
-      this.setState({
-         [name]: value,
-      });
-   };
-
-   isSafe = () => {
-      const { userid, password, email, disabled } = this.state;
-      const passwordMatch = password.indexOf(userid);
-
-      this.setState({
-         disabled: !passwordMatch ? true : false,
-      });
-
-      // const passwordMatch = this.state.password.indexOf(this.state.userid);
-      // const emailMatch = this.state.email.indexOf("@");
-      // const alphabet = /(?=.*[a-z])(?=.*[A-Z])/;
-
-      // (this.state.password.length > 5,
-      // alphabet.test(this.state.password),
-      // passwordMatch,
-      // emailMatch)
-      //    ? this.setState({ disabled: false })
-      //    : this.setState({ disabled: true });
-   };
-
    render() {
-      const { userid, password, email, disabled } = this.state;
-      return (
-         <form onChange={this.isSafe}>
-            <input name="userid" value={userid} onChange={this.handleChange}></input>
-            <input name="password" value={password} onChange={this.handleChange}></input>
-            <input name="email" value={email} onChange={this.handleChange}></input>
-            <button type="button" disabled={disabled}>
-               Submit
-            </button>
-         </form>
-      );
+      return <div>hello World!!</div>;
    }
 }
 
