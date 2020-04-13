fa98f3da6bc9d90b71453d2cc5b0559fc8c34863
pjh <pjh@gmail.com>
2020-04-13 20:01:49 +0900

    aa

---
+++ src/Component/Nav/NavList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavList extends Component {
+   render() {
+      return (
+         <div>
+            <ul>
+               <li mou></li>
+            </ul>
+         </div>
+      );
+   }
+}
+
+export default NavList;
